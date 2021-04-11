9e79539ae3c91714b4a3fb00eebc7f6cec9e0383
karapetyan <karapetyan@inftest.ru>
2021-04-11 14:48:58 +0300

    create pdf from dir

---
+++ mainwindow.cpp
@@ -0,0 +1,107 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() { delete ui; }
+
+
+void MainWindow::createPdf(QString pdfFullName, QStringList filesName) {
+    QPrinter printer;
+    printer.setOutputFormat(QPrinter::PdfFormat);
+    printer.setOutputFileName(pdfFullName);
+    printer.setFullPage(true);
+    QPainter painter;
+
+    for (const auto &str: filesName) {
+        addPage(printer, painter, str);
+    }
+    painter.end();
+}
+
+void MainWindow::addPage(QPrinter &printer, QPainter &painter, QString fileName) {
+    static int numPage = 0;
+    ++numPage;
+    qDebug() << fileName;
+    QPixmap pixmap;
+    if (!pixmap.load(fileName)) {
+        qWarning() << "can't load file:" << fileName;
+        return;
+    }
+    qDebug() << pixmap.size();
+    qDebug() << printer.setPageSize(QPageSize(pixmap.size() * .75, QPageSize::Point));
+    if (numPage == 1) {
+        if (! painter.begin(&printer)) {
+            qWarning("failed to open file, is it writable?");
+            return;
+        }
+    } else {
+        qDebug() << printer.newPage();
+    }
+    painter.drawPixmap(0, 0, pixmap);
+}
+
+void MainWindow::on_btnSelectDir_clicked() {
+    QString dirStr = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+                                                    "",
+                                                    QFileDialog::ShowDirsOnly
+                                                    | QFileDialog::DontResolveSymlinks);
+    ui->leIn->setText(dirStr);
+    QDir dir(dirStr);
+    dir.setFilter(QDir::Files | QDir::NoSymLinks);
+    QFileInfoList list = dir.entryInfoList();
+    qDebug() << "     Bytes Filename";
+    for (int i = 0; i < list.size(); ++i) {
+        QFileInfo fileInfo = list.at(i);
+        qDebug() << QString("%1 %2").arg(fileInfo.size(), 10)
+                                    .arg(fileInfo.fileName());
+    }
+}
+
+void MainWindow::on_btnSelectFile_clicked() {
+    QString fileStr = QFileDialog::getOpenFileName(this, tr("Select file"), "",
+                                                  tr("Zip (*.zip, *.cbz);;Any (*.*)"));
+    ui->leIn->setText(fileStr);
+}
+
+void MainWindow::on_pushButton_clicked() {
+    QString fileStr = QFileDialog::getSaveFileName(this, tr("Select file"), "",
+                                                  tr("PDF (*.pdf)"));
+    ui->leOutFile->setText(fileStr);
+}
+
+void MainWindow::on_btnStart_clicked() {
+    if (ui->leIn->text().isEmpty()) {
+        QMessageBox::warning(this, "Select file", "Select dir or file for reading");
+        return;
+    }
+    if (ui->leOutFile->text().isEmpty()) {
+        QMessageBox::warning(this, "Select file", "Select out file");
+        return;
+    }
+    QFileInfo fileInfo(ui->leIn->text());
+    QStringList selectFiles;
+    if (fileInfo.isDir()) {
+        QDir dir(ui->leIn->text());
+        dir.setFilter(QDir::Files | QDir::NoSymLinks);
+        QFileInfoList list = dir.entryInfoList();
+        for (int i = 0; i < list.size(); ++i) {
+            selectFiles.append(list.at(i).absoluteFilePath());
+        }
+    } else {
+        QMessageBox::warning(this, "amm", "must todo");
+    }
+    createPdf(ui->leOutFile->text(), selectFiles);
+    ui->statusbar->showMessage(QString("File %1 created!")
+                               .arg(QFileInfo(ui->leOutFile->text()).fileName()));
+}
+
+
---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QPixmap>
+#include <qprinter.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btnSelectDir_clicked();
+
+    void on_btnStart_clicked();
+
+    void on_btnSelectFile_clicked();
+
+    void on_pushButton_clicked();
+
+private:
+    void createPdf(QString pdfFullName, QStringList filesName);
+    void addPage(QPrinter &printer, QPainter &painter, QString fileName);
+    Ui::MainWindow *ui;
+};
