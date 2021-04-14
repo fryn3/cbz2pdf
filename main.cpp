0cc8a57e671338e34c59d62da09df80de33a0b56
karapetyan <karapetyan@inftest.ru>
2021-04-14 19:54:20 +0300

    add command line parser

--- main.cpp
+++ main.cpp
@@ -1,11 +1,47 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+    QCommandLineOption coefOption({"k", "coef"}, "Set coefficient", "coef");
+    QCommandLineOption pathOption({"d", "dir", "f", "file", "p", "path"}, "Input path", "path");
+    QCommandLineOption miniOption({"m", "mini"}, "Without window and auto start");
+    QCommandLineOption startOption({"s", "start"}, "Auto start, if not mini");
+
+    QCommandLineParser parser;
+    parser.addOptions({coefOption, pathOption, miniOption, startOption});
+    parser.process(a.arguments());
+
+    if (parser.isSet(miniOption)) {
+        if (!parser.isSet(pathOption)) {
+            Q_ASSERT(false && "must be path");
+            return 1;
+        }
+        Cbz2pdf c;
+        c.setDirOrZipName(parser.value(pathOption));
+        if (parser.isSet(coefOption)) {
+            c.setK(parser.value(coefOption).toDouble());
+        }
+        QObject::connect(&c, &Cbz2pdf::finished, &a, &QApplication::quit);
+        c.start();
+        return a.exec();
+    } else {
+        MainWindow w;
+        if (parser.isSet(coefOption)) {
+            w.setK(parser.value(coefOption).toDouble());
+        }
+        if (parser.isSet(pathOption)) {
+            w.setPath(parser.value(pathOption));
+        }
+        if (parser.isSet(startOption)) {
+            w.start();
+        }
+        w.show();
+        return a.exec();
+    }
 }
