19aeb72844e66fbe3da06f51b7d3b23a6eb79c20
karapetyan <karapetyan@inftest.ru>
2021-04-15 19:02:23 +0300

    add comments for cbz2pdf.h

--- cbz2pdf/cbz2pdf.h
+++ cbz2pdf/cbz2pdf.h
@@ -16,29 +16,88 @@
 class Cbz2pdfWorker;
 class Cbz2pdfPrivate;
 
+/*!
+ * \brief The Cbz2pdf class переводит картинки в формат PDF.
+ */
 class CBZ2PDF_EXPORT Cbz2pdf : public QObject {
     Q_OBJECT
 public:
+
+    /*!
+     * \brief The CodeError enum возвращается вместе с сигналом finished.
+     */
     enum CodeError {
-        NoError,
-        ErrStoped,
-        ErrFileOut,
-        ErrFileIn,
+        NoError,        ///< Ошибок нет.
+        ErrStoped,      ///< Процесс остановлен (устаревший).
+        ErrFileIn,      ///< Ошибка с входными файлами.
+        ErrFileOut,     ///< Ошибка с выходными файлами.
 
         CodeErrorCount
     };
+
     explicit Cbz2pdf(QObject *parent = nullptr);
     virtual ~Cbz2pdf();
+
 public slots:
+
+    /*!
+     * \brief Устанавливает коэффициент для размера страницы.
+     *
+     * Не знаю почему, при вызове QPrinter::setPageSize необходимо передавать
+     * размер QPixmap::size() * K. Причем если разрешение экрана fullHD, то
+     * K = 0.75 (значение по умолчанию), если разрешение экрана 4K, то нужно
+     * устанавливать значение K = 0.5.
+     * \param k - коэффициент размера страницы.
+     */
     void setK(double k);
+
+    /*!
+     * \brief Устанавливает путь для парсинга.
+     * \param dirOrZipName - путь к архиву или к папке, где будет поиск
+     * картинок.
+     */
     void setDirOrZipName(QString dirOrZipName);
+
+    /*!
+     * \brief Запускает процесс создания PDF файлов.
+     */
     void start();
+
+    /*!
+     * \brief Останавливает процесс.
+     */
     void stop();
+
 signals:
+
+    /*!
+     * \brief Испускается после вызова start().
+     */
     void started();
+
+    /*!
+     * \brief Испускается при завершении создании файлов.
+     * \param codeError - возвращает код ошибки enum CodeError.
+     */
     void finished(int codeError);
+
+    /*!
+     * \brief Испускается после вызова stop().
+     *
+     * После этого сигнала не вызывается сигнал finished().
+     */
     void stoped();
+
+    /*!
+     * \brief Процесс выполнения.
+     * \param process - число в пределах от 0 до 1.
+     */
     void process(double process);
+
+    /*!
+     * \brief Выводит сообщения с какими файлами работает.
+     * \param msg - текст сообщения.
+     */
     void logMsg(QString msg);
 private:
     Cbz2pdfWorker *_worker;
